9aa202c905d346f50c4b62915bd5fda67d4b2b12
Agalvf <alejito20401@hotmail.com>
2022-04-09 16:42:18 -0500

    Frontend Vue

--- app/app.py
+++ app/app.py
@@ -1,21 +1,24 @@
-
-from crypt import methods
-import mimetypes
-from urllib import response
-from flask import Flask, request
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS
 from flask_pymongo import PyMongo
-from flask import render_template
-
 from dotenv import load_dotenv
+from bson import json_util, ObjectId
 
 load_dotenv('./.flaskenv')
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 app.config['MONGO_URI'] = "mongodb://localhost/analisisyDiseñoAlgoritmos";
+
+CORS(app,resources={r"/*":{'origins':"*"}})
 mongo = PyMongo(app)
 
-@app.route("/user",methods=['POST'])
-def create_user():
+@app.route("/shark",methods=["GET"])
+def shark():
+    return("Shark 🦈!")
+
+""" @app.route("/user",methods=['POST'])
+def create_grafos():
     
     username = request.json['username']
     email = request.json['email']
@@ -37,12 +40,29 @@ def create_user():
         }
         return response
     else:
-        {'message': 'received'}
+        return not_found()
 
-@app.route("/users",methods=['GET'])
-def get_users():
-    users = mongo.db.grafos.find()
+@app.route("/user",methods=['GET'])
+def get_grafos():
+    grafos = mongo.db.grafos.find()
     response = json_util.dumps(grafos)
-    return Respose(response,mimetype='application/json')
+    return Response(response,mimetype='application/json')
+
+@app.route("/user/<id>",methods=["GET"])
+def get_users(id):
+    grafo = mongo.db.grafos.find_one({'_id': ObjectId(id)})
+    response = json_util.dumps(grafo)
+    return Response(response, mimetype="application/json") """
+
+@app.errorhandler(404)
+def not_found(error=None):
+    response = jsonify({
+        'message': 'Resource Not Found: ' + request.url,
+        'status': 404
+    })
+    response.status_code = 404
+    return response
+
+
 if __name__ == '__main__':
     app.run()
